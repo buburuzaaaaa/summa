3447f8ae8e37b85717c414873380aa5ee41c5957
buburuzaaaaa <andrew4joy2015@gmail.com>
2023-08-07 14:58:34 -0700

    done

---
+++ script.js
@@ -0,0 +1,7 @@
+function clicker (){
+    // let clicked = document.getElementById("me");
+    // if(clicked == true){
+    //     alert("Get off my page bozo");
+    // }
+    alert("Get off my page bozo");
+}
